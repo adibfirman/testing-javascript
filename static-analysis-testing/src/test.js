6a02cebba6bc052dedcd137f404cdcf7fb597659
Adib Firman <dib.firman@gmail.com>
2019-02-10 15:27:47 +0700

    adding src folder

---
+++ static-analysis-testing/src/test.js
@@ -0,0 +1,11 @@
+const name = 'Adib'
+typeof name === 'string'
+
+if (!ServiceWorker in navigator) {
+  // you have an old browser
+}
+
+const greeting = 'hello world'
+console.log(`the greeting is ${greeting}`)
+
+[1,2,3].forEach(n => console.log(n));
