c27d5b74828511f400e7ea80b3df52fd9ea66036
Adib Firman <dib.firman@gmail.com>
2019-02-11 11:22:24 +0700

    adding prettier

--- static-analysis-testing/src/test.js
+++ static-analysis-testing/src/test.js
@@ -1,11 +1,10 @@
-const name = 'Adib'
-typeof name === 'string'
+const name = "Adib";
+typeof name === "string";
 
-if (!('serviceWorker' in navigator)) {
+if (!("serviceWorker" in navigator)) {
   // you have an old browser
 }
 
-const greeting = 'hello world'
-console.log(`the greeting is ${greeting}`)
-
-;[1, 2, 3].forEach(n => console.log(n));
+const greeting = "hello world";
+console.log(`the greeting is ${greeting}`);
+[1, 2, 3].forEach(n => console.log(n));
