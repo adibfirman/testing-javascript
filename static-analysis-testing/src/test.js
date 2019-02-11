1e232b0df4e4825f9e8138301b60719a5d2d5ba0
Adib Firman <dib.firman@gmail.com>
2019-02-11 11:55:15 +0700

    adding script validate for get list all different from prettier

--- static-analysis-testing/src/test.js
+++ static-analysis-testing/src/test.js
@@ -5,7 +5,4 @@ if (!('serviceWorker' in navigator)) {
   // you have an old browser
 }
 
-const greeting = 'hello world'
-console
-  .log(`the greeting is ${greeting}`)
-  [(1, 2, 3)].forEach(n => console.log(n))
+;[(1, 2, 3)].forEach(n => console.log(n))
