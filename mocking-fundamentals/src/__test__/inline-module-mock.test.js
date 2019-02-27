1e0436ab717c8ad3719a62b5f578548357a28bea
Adib Firman <dib.firman@gmail.com>
2019-02-27 17:46:02 +0700

    add inline module mock

--- mocking-fundamentals/src/__test__/inline-module-mock.test.js
+++ mocking-fundamentals/src/__test__/inline-module-mock.test.js
@@ -1,10 +1,13 @@
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
-test('return winner', () => {
-  jest.spyOn(utils, 'getWinner')
-  utils.getWinner.mockImplementation((p1, p2) => p1)
+jest.mock('../utils', () => {
+  return {
+    getWinner: jest.fn((p1, p2) => p1)
+  }
+})
 
+test('return winner', () => {
   const winner  = thumbWar('Adib Firman', 'Firman Ken')
   expect(winner).toBe('Adib Firman')
   expect(utils.getWinner).toHaveBeenCalledTimes(2)
@@ -13,5 +16,5 @@ test('return winner', () => {
   expect(utils.getWinner).toHaveBeenNthCalledWith(2, 'Adib Firman', 'Firman Ken')
 
   // cleanup
-  utils.getWinner.mockRestore()
+  utils.getWinner.mockReset()
 })
