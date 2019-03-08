c1d3083a3b2c86be5b91b83b206ceb3efece115a
Adib Firman <dib.firman@gmail.com>
2019-03-08 16:55:46 +0700

    start tdd and tdd markup

---
+++ react-testing-library-course/src/__tests__/TDD/tdd-01-markup.test.js
@@ -0,0 +1,14 @@
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+
+import React from 'react'
+import {render} from 'react-testing-library'
+import {Editor} from '../../post-editor-01-markup'
+
+test('renders a form with title, content, tags, and submit button', () => {
+  const {getByLabelText, getByText} = render(<Editor />)
+  getByLabelText(/title/i)
+  getByLabelText(/content/i)
+  getByLabelText(/tags/i)
+  getByText(/submit/i)
+})
